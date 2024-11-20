7cc713b9a218ff6418dcdb46d0bcf5256f7e7943
MNiedbal <mniedbal@sygnity.pl>
2024-11-20 02:30:33 +0100

    init

---
+++ src/app/rect/rect.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  standalone: true,
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
+})
+export class RectComponent {
+
+  @Input({required: true}) size!: {width: string; height: string};
+  @Output() sizeChange = new EventEmitter<{width: string; height: string}>();
+
+  onReset() {
+    console.log("resize");
+    this.sizeChange.emit({
+      width: '200',
+      height: '100'
+    })
+  }
+}
