d4b406591c8202301c6332ed0030f8e90ac9ebe2
MNiedbal <mniedbal@sygnity.pl>
2024-11-20 02:33:29 +0100

    easier way of two way binding

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, model, Output} from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,12 +9,14 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class RectComponent {
 
-  @Input({required: true}) size!: {width: string; height: string};
-  @Output() sizeChange = new EventEmitter<{width: string; height: string}>();
+  // @Input({required: true}) size!: {width: string; height: string};
+  // @Output() sizeChange = new EventEmitter<{width: string; height: string}>();
+
+  size = model.required<{width: string; height: string}>();
 
   onReset() {
     console.log("resize");
-    this.sizeChange.emit({
+    this.size.set({
       width: '200',
       height: '100'
     })
